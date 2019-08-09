d1d4aa350874b1ab7517b6762daa236cad2522f9
Lucas Tolan <beniamin.tolan@gmail.com>
2019-08-09 08:53:34 +0200

    prop greeting added to hello component

--- src/Hello.js
+++ src/Hello.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './Hello.css';
 
+
 function Hello() {
     return (
-        <div>
+        <div className='f1 tc'>
             <h1>Hello World from Blois</h1>
             <p>Welocme to React</p>
         </div>
