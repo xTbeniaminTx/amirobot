64e3c691eb24fc760bf204e87189bbedf4ee48da
Lucas Tolan <beniamin.tolan@gmail.com>
2019-08-09 10:19:53 +0200

    after destructuring lite method

--- src/Card.js
+++ src/Card.js
@@ -3,12 +3,13 @@ import React from 'react';
 // import './Card.css';
 
 const Card = (props) => {
+    const {name, email, id} = props;
     return (
         <div className='tc bg-light-green dib br3 pa3 ma2 grow shadow-5'>
-            <img src={`https://robohash.org/${props.id}?size=200x200`} alt={props.name}/>
+            <img src={`https://robohash.org/${id}?size=200x200`} alt={props.name}/>
             <div>
-                <h2>{props.name}</h2>
-                <p>{props.email}</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     );
