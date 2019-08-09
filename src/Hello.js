1208cce022b0410a05d1d7aea23980bdb08c92c5
Lucas Tolan <beniamin.tolan@gmail.com>
2019-08-09 08:39:59 +0200

    #1:first component

---
+++ src/Hello.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Hello.css';
+
+function Hello() {
+    return (
+        <div>
+            <h1>Hello World from Blois</h1>
+            <p>Welocme to React</p>
+        </div>
+    );
+}
+
+export default Hello;
