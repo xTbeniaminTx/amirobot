1c62ed42fd13a9f6d3da44e0f733be77e8f71682
Lucas Tolan <beniamin.tolan@gmail.com>
2019-08-09 10:21:11 +0200

    after destructuring full method

--- src/Card.js
+++ src/Card.js
@@ -2,11 +2,10 @@ import React from 'react';
 
 // import './Card.css';
 
-const Card = (props) => {
-    const {name, email, id} = props;
+const Card = ({name, email, id}) => {
     return (
         <div className='tc bg-light-green dib br3 pa3 ma2 grow shadow-5'>
-            <img src={`https://robohash.org/${id}?size=200x200`} alt={props.name}/>
+            <img src={`https://robohash.org/${id}?size=200x200`} alt={name}/>
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
