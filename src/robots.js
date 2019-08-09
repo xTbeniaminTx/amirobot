b507140a4e4f465faab86b0c203972b4703f58d9
Lucas Tolan <beniamin.tolan@outlook.com>
2019-08-09 23:48:14 +0200

    cardlist componont

--- src/robots.js
+++ src/robots.js
@@ -43,7 +43,7 @@ export const robots = [
   },
   {
     id: 8,
-    name: 'Nicholas Runolfsdottir V',
+    name: 'Nicholas Runolf V',
     username: 'Maxime_Nienow',
     email: 'Sherwood@rosamond.me'
   },
