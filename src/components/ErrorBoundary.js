3776b9e9a3c1490d032dce7ed5787ab8f5046930
btolan-karudev <beniamin.tolan@outlook.com>
2020-02-13 23:04:51 +0100

    errorBoundray

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+class ErrorBoundary extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hasError: false
+        }
+    }
+
+    componentDidCatch(error, errorInfo) {
+        this.setState({hasError: true})
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return <h1>
+                OOOoooosps. This is not good!
+            </h1>
+        }
+        return this.props.children
+    }
+}
+
+export default ErrorBoundary;
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundary from "../components/ErrorBoundary";
 import './App.css';
 
 
@@ -40,9 +41,9 @@ class App extends Component {
                     <h1 className="f1">RoboticAmi</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
                     <Scroll>
-                        < CardList
-                            robots={filteredRobots}
-                        />
+                        <ErrorBoundary>
+                            < CardList robots={filteredRobots}/>
+                        </ErrorBoundary>
                     </Scroll>
                 </div>
             );
